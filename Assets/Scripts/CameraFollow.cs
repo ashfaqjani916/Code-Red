a4a7599a49313118daa145bd4281b7c72edeab27
ashfaqjani916 <ashfaqjani916@gmail.com>
2023-07-30 01:11:20 +0530

    basic features

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public 
+    public Transform player;
+    public Vector3 offset;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called once per frame, after all Update methods have been called
+    void LateUpdate()
     {
-        
+        Vector3 targetPosition = player.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
